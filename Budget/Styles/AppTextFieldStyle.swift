25920aa7c6ea53a5f77f38b5c414ca0c9e446d88
Leo Sorg <sorgleo@gmail.com>
2025-09-16 08:01:23 -0300

    removed background and useless files

--- Budget/Styles/AppTextFieldStyle.swift
+++
@@ -1,9 +0,0 @@
-// This file is completely REMOVED
-// All keyboard functionality is now in KeyboardManager.swift
-//
-// DO NOT ADD ANYTHING TO THIS FILE
-// Use KeyboardManager.swift instead
-
-import SwiftUI
-
-// This file intentionally left empty to avoid conflicts
