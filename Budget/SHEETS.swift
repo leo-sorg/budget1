038bb4e2403d0b147ac0f343af2f98946a224600
Leo Sorg <sorgleo@gmail.com>
2025-09-16 21:05:49 -0300

    fix link to api

--- Budget/SHEETS.swift
+++ Budget/SHEETS.swift
@@ -2,7 +2,7 @@
 import Foundation
 
 let SHEETS = SheetsClient(
-    baseURL: URL(string: "https://script.google.com/macros/s/AKfycbw8aOjyZDuv9Nc5zCVSjhHysPxAYyx3pSaS1cFNGEtACjIVyN4gxJ4eE1Ku_zLPErdt/exec")!,
+    baseURL: URL(string: "https://script.google.com/macros/s/AKfycbxMEQUf9_0W5uHChi9Ta7P1iMgiHdZZLcyfPOI0zfRlusbRRo9kpGdLHQsbPrOQS7mn/exec")!,
     secret: "budget2761"
 )
 
