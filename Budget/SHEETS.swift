0ac0abbf8cda1640197fb56b1bf2171ec5bf570b
Leo (Mac) <sorgleo@gmail.com>
2025-09-24 07:59:09 -0300

    fixing cache

--- Budget/SHEETS.swift
+++ Budget/SHEETS.swift
@@ -3,6 +3,6 @@ import Foundation
 import UIKit
 
 let SHEETS = SheetsClient(
-    baseURL: URL(string: "https://script.google.com/macros/s/AKfycbzjhsU2UnLnL7vv3s9QnsTIXb8Y8EkvZuKwoZDUAQLuGDZYOD5-qRuifqCAWKTdUP8Q/exec")!,
+    baseURL: URL(string: "https://script.google.com/macros/s/AKfycbxz3MSmbhUwZU3nv5rjiH0ccZjlM_uqAGzJvjVzaCpiSIvx4X-oLmSjBlYaBQ3G5ejF/exec")!,
     secret: "budget2761"
 )
